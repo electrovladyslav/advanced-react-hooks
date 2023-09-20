4beddd5f0ef4bafb78ed6a0fb367592dff324ee5
Vladyslav Yermolin <vladyslav.yermolin@masterofcode.com>
2023-09-20 13:46:13 +0300

    Task 1.3. is done (UseReduced action as a function)

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,14 +4,22 @@
 import * as React from 'react'
 import {useReducer} from "react";
 
-const countReducer = (stateCount, action) => ({...stateCount, ...action})
+const countReducer = (state, action) => {
+  if (typeof action === 'function') {
+    return action(state)
+  } else {
+    return {...state, ...action}
+  }
+}
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () => setState({count: count + step})
+  // const increment = () => setState({count: count + step})
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
 
   return <button onClick={increment}>{count}</button>
 }
