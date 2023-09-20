42e4c25ea3ee335d52d50a014d911914970d3711
Vladyslav Yermolin <vladyslav.yermolin@masterofcode.com>
2023-09-20 13:55:42 +0300

    Task 1.4. is done (UseReduced with action.type - redux style)

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,21 +5,27 @@ import * as React from 'react'
 import {useReducer} from "react";
 
 const countReducer = (state, action) => {
-  if (typeof action === 'function') {
-    return action(state)
-  } else {
-    return {...state, ...action}
+  debugger
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        count: state.count + action.step
+      }
+    default:
+      throw new TypeError('Wrong action type');
   }
 }
 
-function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = useReducer(countReducer, {
+function Counter({initialCount = 0, step = 2.5}) {
+  const [state, dispatch] = useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
   // const increment = () => setState({count: count + step})
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  // const increment = () =>
+  //   setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
 
   return <button onClick={increment}>{count}</button>
 }
