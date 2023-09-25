7d752ed8e861ae7575c9a30a6770b59616e1d35b
Vladyslav Yermolin <vladyslav.yermolin@masterofcode.com>
2023-09-25 23:09:38 +0300

    Task 6 is done (useDebugValue)

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,11 +2,17 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import {useDebugValue} from "react";
+
+function formatQueryDebug({query, state}) {
+  return `\`${query}\` => ${state}`;
+}
 
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
   // 🐨 call React.useDebugValue here.
   // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
+  useDebugValue({query, state}, formatQueryDebug)
 
   React.useEffect(() => {
     let mounted = true
